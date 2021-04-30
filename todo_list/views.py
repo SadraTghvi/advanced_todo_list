f9fa01f059089a4189b7afc6e91a20720bf7295f
sadra <sadrataghavi1383@gmail.com>
2021-04-30 16:36:22 +0430

    updated functionality for editing item and form for each item

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import redirect, render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import *
+from django.urls import reverse_lazy
+from .models import Task
 
 # Create your views here.
 
-def index(request):
-    return render(request,"index.html")
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "todo_list/task.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
