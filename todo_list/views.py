f352edd2d7fc6641a111d118637b43ad1b9cbee7
sadra <sadrataghavi1383@gmail.com>
2021-05-01 16:19:28 +0430

    class and form for page

--- todo_list/views.py
+++ todo_list/views.py
@@ -7,6 +7,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import *
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 from .models import Task
 
@@ -19,6 +22,22 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("tasks")
+
+class RegisterPage(FormView):
+    template_name = "todo_list/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("tasks")
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage,self).form_valid(form)
+
+    def get(self,*args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("tasks")
+        return super(RegisterPage,self).get(*args,**kwargs)
 class CustomLogoutView(LogoutView):
     template_name = "todo_list/login.html"
     fields = "__all__"
